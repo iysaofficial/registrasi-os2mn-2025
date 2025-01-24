533e64cd5e6145a90109ddaa3339a17d054632fc
iysaofficial <youngscientist.iysa@gmail.com>
2025-01-24 16:24:32 +0700

    first commit

---
+++ pages/thankyouindopage.js
@@ -0,0 +1,19 @@
+const Thankyou = () => {
+  return (
+    <>
+      <section className="thankyou">
+        <div>
+          <h1 className="text-sm md:text-lg lg:text-4xl">
+            Terima kasih telah mendaftar! Kami menghargai partisipasi Anda dan
+            menantikan keterlibatan Anda.
+          </h1>
+          <a href="/" className="btn btn-action">
+          Kembali ke menu pendaftaran
+          </a>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Thankyou;
